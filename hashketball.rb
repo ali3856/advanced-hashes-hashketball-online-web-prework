041d6395a40024eedd41e085ccccce028e15b091
witty-banner-1950 <asimsali95@gmail.com>
2019-06-27 19:25:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,101 +2,101 @@
 require "pry"
 
 
-def game_hash 
+def game_hash
   game_hash = {
-    :home => {:team_name => 'Brooklyn Nets', :colors => ["Black", "White"], :players => {"Alan Anderson" => 
+    :home => {:team_name => 'Brooklyn Nets', :colors => ["Black", "White"], :players => {"Alan Anderson" =>
       {:number => 0,
       :shoe => 16,
       :points => 22,
-      :rebounds => 12, 
-      :assists => 12, 
-      :steals => 3, 
-      :blocks => 1, 
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
       :slam_dunks => 1},
-      "Reggie Evans" =>  
-      {:number => 30, 
+      "Reggie Evans" =>
+      {:number => 30,
       :shoe => 14,
       :points => 12,
-      :rebounds => 12, 
-      :assists => 12, 
-      :steals => 12, 
-      :blocks => 1, 
-      :slam_dunks => 7}, 
-      "Brook Lopez" => 
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 12,
+      :blocks => 1,
+      :slam_dunks => 7},
+      "Brook Lopez" =>
       {:number => 11,
       :shoe => 17,
       :points => 17,
-      :rebounds => 19, 
-      :assists => 10, 
-      :steals => 3, 
-      :blocks => 1, 
+      :rebounds => 19,
+      :assists => 10,
+      :steals => 3,
+      :blocks => 1,
       :slam_dunks => 15},
-      "Mason Plumlee" => 
+      "Mason Plumlee" =>
       {:number => 1,
       :shoe => 19,
       :points => 26,
-      :rebounds => 12, 
-      :assists => 6, 
-      :steals => 3, 
-      :blocks => 8, 
+      :rebounds => 12,
+      :assists => 6,
+      :steals => 3,
+      :blocks => 8,
       :slam_dunks => 5},
       "Jason Terry" =>
       {:number => 31,
       :shoe => 15,
       :points => 19,
-      :rebounds => 2, 
-      :assists => 2, 
-      :steals => 4, 
-      :blocks => 11, 
+      :rebounds => 2,
+      :assists => 2,
+      :steals => 4,
+      :blocks => 11,
       :slam_dunks => 1},}},
-    :away => {:team_name => 'Charlotte Hornets', :colors => ["Turquoise", "Purple"], :players => 
-      {"Jeff Adrien" => 
+    :away => {:team_name => 'Charlotte Hornets', :colors => ["Turquoise", "Purple"], :players =>
+      {"Jeff Adrien" =>
       {:number => 4,
       :shoe => 18,
       :points => 10,
-      :rebounds => 1, 
-      :assists => 1, 
-      :steals => 2, 
-      :blocks => 7, 
+      :rebounds => 1,
+      :assists => 1,
+      :steals => 2,
+      :blocks => 7,
       :slam_dunks => 2},
-      "Bismak Biyombo" =>  
-      {:number => 0, 
+      "Bismak Biyombo" =>
+      {:number => 0,
       :shoe => 16,
       :points => 12,
-      :rebounds => 4, 
+      :rebounds => 4,
       :assists => 7,
       :steals => 7,
-      :blocks => 15, 
-      :slam_dunks => 10}, 
-      "DeSagna Diop" => 
+      :blocks => 15,
+      :slam_dunks => 10},
+      "DeSagna Diop" =>
       {:number => 2,
       :shoe => 14,
       :points => 24,
-      :rebounds => 12, 
-      :assists => 12, 
-      :steals => 4, 
-      :blocks => 5, 
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 4,
+      :blocks => 5,
       :slam_dunks => 5},
-      "Ben Gordon" => 
+      "Ben Gordon" =>
       {:number => 8,
       :shoe => 15,
       :points => 33,
-      :rebounds => 3, 
-      :assists => 2, 
-      :steals => 1, 
-      :blocks => 1, 
+      :rebounds => 3,
+      :assists => 2,
+      :steals => 1,
+      :blocks => 1,
       :slam_dunks => 0},
       "Brendan Haywood" =>
       {:number => 33,
       :shoe => 15,
       :points => 6,
-      :rebounds => 12, 
-      :assists => 12, 
-      :steals => 22, 
-      :blocks => 5, 
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 22,
+      :blocks => 5,
       :slam_dunks => 12},}}
     }
-end 
+end
 
 
 # for the name passed in to the argument, return the shots scored for the #played whos name it is.
@@ -105,43 +105,43 @@ end
 #        value           key           key               key       value #["Black", "White"], :players => {"Alan Anderson" => {:number => 0,etc.
 
 def num_points_scored(name)
-  #binding.pry 
+  #binding.pry
   game_hash
-  if game_hash[:home][:players].key?(name) 
+  if game_hash[:home][:players].key?(name)
   #binding.pry
   game_hash[:home][:players][name][:points]
-  
-  elsif game_hash[:away][:players].key?(name) 
-  #binding.pry 
+
+  elsif game_hash[:away][:players].key?(name)
+  #binding.pry
   game_hash[:away][:players][name][:points]
 end
 end
 
 def shoe_size(name)
-    if game_hash[:home][:players].key?(name) 
+    if game_hash[:home][:players].key?(name)
   #binding.pry
   game_hash[:home][:players][name][:shoe]
-  
-    elsif game_hash[:away][:players].key?(name) 
-  #binding.pry 
+
+    elsif game_hash[:away][:players].key?(name)
+  #binding.pry
   game_hash[:away][:players][name][:shoe]
-  end 
-end 
+  end
+end
 
 def team_colors(team_name)
-  if game_hash[:home].has_value?(team_name) 
+  if game_hash[:home].has_value?(team_name)
     game_hash[:home][:colors]
  elsif game_hash[:away].has_value?(team_name)
     game_hash[:away][:colors]
-  end 
-end 
+  end
+end
 
 def team_names
   team_array = []
     team_array << game_hash[:home][:team_name]
     team_array << game_hash[:away][:team_name]
   team_array
-end 
+end
 
 #  hash            key       key           val               key
   # game_hash = {:home => {:team_name => 'Brooklyn Nets', :colors =>
@@ -152,15 +152,15 @@ def player_numbers(name)
   numbers = []
 
 
-  if game_hash[:home][:team_name] == name 
-    game_hash[:home][:players].each do |key, value| 
-      numbers << value[:number] 
-      end 
-     
-  elsif game_hash[:away][:team_name] == name 
+  if game_hash[:home][:team_name] == name
+    game_hash[:home][:players].each do |key, value|
+      numbers << value[:number]
+      end
+
+  elsif game_hash[:away][:team_name] == name
     game_hash[:away][:players].each do |key, value|
-      numbers << value[:number] 
-    end 
+      numbers << value[:number]
+    end
   end
-  numbers 
+  numbers
 end 
