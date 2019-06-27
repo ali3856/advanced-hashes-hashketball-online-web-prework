157a453cd1a6ccdb5a3feb9fd915cff82178d963
witty-banner-1950 <asimsali95@gmail.com>
2019-06-27 17:02:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,5 +185,16 @@ def player_stats(name)
 end
 
 def big_shoe_rebounds
-
-end
+   biggest = 0
+   rebounds = 0
+   game_hash.each do |home_away, keys|
+     keys[:players].each do |player|
+       size = player[:shoe]
+       if size > biggest
+         biggest = size
+         rebounds = player[:rebounds]
+       end
+     end
+   end
+   rebounds
+ end
